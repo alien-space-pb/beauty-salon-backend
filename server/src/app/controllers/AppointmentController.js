cb26371f812b4422760ec6e5c00da1be26a824ae
AlienDev66 <59874434+AlienDev66@users.noreply.github.com>
2020-07-16 00:02:30 +0100

    :sparkles: Começando e quase termiando a feature de NOTIFICAÇÃO DE NOVOS AGENDAMENTOS.

--- server/src/app/controllers/AppointmentController.js
+++ server/src/app/controllers/AppointmentController.js
@@ -1,8 +1,10 @@
 import Appointment from "../models/Appointment";
-import { startOfHour, parseISO, isBefore } from 'date-fns';
+import { startOfHour, parseISO, isBefore, format } from 'date-fns';
+import pt from 'date-fns/locale/pt'
 import * as Yup from "yup";
 import User from "../models/User";
 import File from "../models/File";
+import Notification from '../schemas/Notification';
 
 class AppointmentController {
 
@@ -84,7 +86,23 @@ class AppointmentController {
         user_id: req.userId,
         provider_id,
         date:hourStart,
-    })
+    });
+
+    /**
+     * Notificar prestador de serviço
+     */
+
+     const user = await User.findByPk(req.userId);
+     const formattedDate = format(
+       hourStart,
+       "'dia' dd 'de' MMMM', às' H:mm'h'",
+       { locale: pt }
+       )
+
+     await Notification.create({
+       content: `Novo agendamento de ${user.name} para dia ${formattedDate}`,
+       user: provider_id,
+     })
 
     return res.json(appointment);
   }
