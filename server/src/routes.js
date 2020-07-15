14c3aaee92b1feec395ad23feadf8362f322a502
AlienDev66 <59874434+AlienDev66@users.noreply.github.com>
2020-07-15 23:06:12 +0100

    :sparkles: Adicionando listagem agendada do prestador de serviço.

--- server/src/routes.js
+++ server/src/routes.js
@@ -7,6 +7,8 @@ import FileController from './app/controllers/FileController';
 import SessionController from './app/controllers/SessionController';
 import ProviderController from './app/controllers/ProviderController';
 import AppointmentController from './app/controllers/AppointmentController';
+import ScheduleController from './app/controllers/ScheduleController';
+
 
 import authMiddleware from './app/middlewares/auth';
 
@@ -25,6 +27,8 @@ routes.get('/providers', ProviderController.index);
 
 routes.get('/appointments', AppointmentController.index)
 routes.post('/appointments', AppointmentController.store)
+
+routes.get('/schedule', ScheduleController.index);
 
 
 routes.post('/files', upload.single('file'), FileController.store)
