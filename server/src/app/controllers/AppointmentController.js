e0d278732bf28dff84c7475a9b69b90d7d4f36b7
AlienDev66 <59874434+AlienDev66@users.noreply.github.com>
2020-07-21 23:04:09 +0100

    :sparkles: Adicionando e configurando envio de emails com o nodemailer utilizando também o MAILTRAP.

--- server/src/app/controllers/AppointmentController.js
+++ server/src/app/controllers/AppointmentController.js
@@ -6,6 +6,8 @@ import User from "../models/User";
 import File from "../models/File";
 import Notification from '../schemas/Notification';
 
+import Mail from '../../lib/Mail';
+
 class AppointmentController {
 
   async index (req, res){
@@ -109,7 +111,15 @@ class AppointmentController {
 
   async delete(req, res){
 
-    const appointment = await Appointment.findByPk(req.params.id);
+    const appointment = await Appointment.findByPk(req.params.id, {
+      include: [ 
+      { 
+        model: User,
+        as: 'provider',
+        attributes: [ 'name', 'email' ]
+      }
+       ],
+    });
 
     if(appointment.user_id !== req.userId){
       return res.status(401).json({ error: "You don't have permission to cancel this appointment"});
@@ -118,12 +128,18 @@ class AppointmentController {
     const dateWithSub = subHours(appointment.date, 2);
 
     if(isBefore(dateWithSub, new Date())){
-      return res.status(401).json({ error: ' You can only cancel appointment 2 hours in advence. '})
+      return res.status(401).json({ error: ' You can only cancel appointment 2 hours in advance. '})
     }
 
     appointment.canceled_at = new Date();
 
     await appointment.save();
+
+    await Mail.sendMail({
+      to: `${ appointment.provider.name } <${ appointment.provider.email }> `,
+      subject: 'Agendamento cancelado',
+      text: 'Você tem um novo cancelamento',
+    })
 
     return res.json(appointment);
   }
---
+++ server/src/config/mail.js
@@ -0,0 +1,12 @@
+export default {
+    host: "smtp.mailtrap.io",
+    port: 2525,
+    secure: false,
+    auth: {
+      user: "5ed1c669ad19b5",
+      pass: "2ec6a726283fff"
+    },
+    default: {
+        form: 'Time GoBarber <noreply@gobarber.com>',
+    },
+}
